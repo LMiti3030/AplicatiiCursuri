4b64a168a0ad6ef0be725c246cefd3cd1f1b8cc6
Laura Mititelu <mititelu_laura@yahoo.com>
2021-04-16 17:39:07 +0300

    RadarTarget

--- Java11OOApproach/src/main/java/Main.java
+++ Java11OOApproach/src/main/java/Main.java
@@ -1,16 +1,38 @@
+import java.sql.SQLOutput;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        FlightPlan berlinToBucharest = new FlightPlan( "BER", "OTP");
-        berlinToBucharest.print();
+
+    //testare FlightPlan
+//    public static void main(String[] args) {
+//        FlightPlan berlinToBucharest = new FlightPlan( "BER", "OTP");
+//        berlinToBucharest.print();
+//        System.out.println();
+//        FlightPlan parisToLondon = new FlightPlan(
+//                "CDG",
+//                "LHR",
+//                LocalDateTime.of(2020, 01, 10 ,23,15),
+//                List.of("CDG", "FARAR", "BAYKA", "DUDES", "LHR"));
+//        parisToLondon.print();
+//    }
+
+    public static void main (String[] args) throws InterruptedException{
+        RadarTarget os791 = new RadarTarget(
+                "OS791",
+                280,
+                "B737-800"
+        );
+        System.out.println(os791.getLabel());
         System.out.println();
-        FlightPlan parisToLondon = new FlightPlan(
-                "CDG",
-                "LHR",
-                LocalDateTime.of(2020, 01, 10 ,23,15),
-                List.of("CDG", "FARAR", "BAYKA", "DUDES", "LHR"));
-        parisToLondon.print();
+
+        os791.changeAltitude(200);
+        for(int i=0; i<5; i++){
+            Thread.sleep(1000);
+            System.out.println(os791.getLabel());
+            System.out.println();
+        }
+
     }
+
 }
